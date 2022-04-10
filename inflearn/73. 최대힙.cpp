df0aa2d2ca0e3fd6bc6a59ecfc2dc174038f61dd
imsoori <ckdks13@gmail.com>
2022-04-10 23:38:44 +0900

    solve

---
+++ inflearn/73. 최대힙.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+
+// 73. 최대힙
+priority_queue <int> pq;
+int main()
+{
+	int n;
+
+	while (true)
+	{
+		scanf_s("%d", &n);
+
+		if (n == -1) { break; }
+		if (n == 0) {
+			if (pq.empty()) { printf("-1\n"); }
+			else {
+				printf("%d\n", pq.top());
+				pq.pop();
+			}
+		}
+		else {
+			pq.push(n);
+		}
+	
+
+	}
+	return 0;
+}

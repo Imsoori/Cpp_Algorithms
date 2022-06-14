a1482874e8025843cefc1726ada22523ba63cd69
Imsoori <ckdks13@gmail.com>
2022-06-15 00:36:18 +0900

    still...

--- Codility/8.2 EquiLeader.cpp
+++ Codility/8.2 EquiLeader.cpp
@@ -1,20 +1,18 @@
 // EquiLeader
 // test result
 // 2022.06.14
-// 
-// 
+// 88%
+// https://app.codility.com/demo/results/trainingFF5UHW-KR8/
 
 #include <map>
 #include <vector>
-
 
 int solution(vector<int>& A) {
     // write your code in C++14 (g++ 6.2.0)
     map <int, int> m_map, m_cmp;
     vector<int> ans;
-    bool check = false;
     int tmpLeader = 0;
-
+    int cmpIdx1, cmpIdx2 = 0;
     for (int i = 0; i < A.size(); i++)
     {
         m_map[A[i]] += 1;
@@ -22,27 +20,34 @@ int solution(vector<int>& A) {
 
     for (int i = 0; i < A.size() - 1; i++)
     {
-        check = false;
+
         if (m_map[A[i]] > 0)
         {
             m_map[A[i]] -= 1;
+            // cout << i << " " << m_map[A[i]] << endl;
         }
 
         m_cmp[A[i]] += 1;
-
-        if (m_cmp[A[i]] > m_cmp.size() / 2)
+        cmpIdx1 = i + 1;
+        for (auto it = m_cmp.begin(); it != m_cmp.end(); ++it)
         {
-            tmpLeader = A[i];
-            check = true;
-        }
-
-        if (check)
-        {
-            if (m_map[tmpLeader] > m_map.size() / 2)
+            if (it->second > cmpIdx1 / 2)
             {
-                ans.push_back(tmpLeader);
+                tmpLeader = it->first;
+                // cout << i << " " << tmpLeader << endl;
+                break;
+
             }
         }
+        cmpIdx2 = A.size() - i - 1;
+
+        if (m_map[tmpLeader] > cmpIdx2 / 2 && m_cmp[tmpLeader] > cmpIdx1 / 2)
+        {
+            //cout << m_map[tmpLeader] << " " << i << endl;
+            ans.push_back(i);
+
+        }
+
 
     }
 

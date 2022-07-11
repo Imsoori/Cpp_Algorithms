cec9b68c076880a6fb39c4fe5bef6ccfd75b72c2
Imsoori <ckdks13@gmail.com>
2022-07-11 23:13:21 +0900

    retry

---
+++ retry/54. 올바른괄호.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+// 54. ¿Ã¹Ù¸¥ °ýÈ£ 
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	string input_str;
+	string result = "YES";
+	vector <char> v;
+
+	cin >> input_str;
+
+	for (int i = 0; i < input_str.size(); i++)
+	{
+
+		if (input_str[i] == '(')
+		{
+
+			v.push_back(input_str[i]);
+
+		}
+		else {
+
+			if (v.empty())
+			{
+				result = "NO";
+				break;
+			}
+			else {
+				v.pop_back();
+			}
+
+		}
+
+
+	}
+
+
+
+	cout << result << endl;
+
+	return 0;
+
+}
